1cc9cd6ee39e3ae9544a37384faf69ae94e2b646
OnggiJonggi <maru403@naver.com>
2026-04-27 20:46:57 +0900

    4.27_1

---
+++ src/main/java/com/axaboutconsulting/AboutConsultingWebApplication.java
@@ -0,0 +1,13 @@
+package com.axaboutconsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutConsultingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboutConsultingWebApplication.class, args);
+	}
+
+}
