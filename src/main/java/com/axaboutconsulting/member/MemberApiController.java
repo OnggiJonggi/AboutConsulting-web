a02773e2c46c65903cf125d52e533eedd229da0f
OnggiJonggi <maru403@naver.com>
2026-05-12 01:31:21 +0900

    5.12_1

--- src/main/java/com/axaboutconsulting/member/MemberApiController.java
+++ src/main/java/com/axaboutconsulting/member/MemberApiController.java
@@ -9,11 +9,33 @@ import org.springframework.web.bind.annotation.RestController;
 import com.axaboutconsulting.global.common.SearchResultVO;
 
 @RestController
-@RequestMapping("/member/api")
+@RequestMapping("/api/member")
 public class MemberApiController {
 
 	@Autowired
 	private MemberService service;
+	
+	/**
+	 * 아이디 중복 확인
+	 * @param member
+	 * @return 정상 200, 이상해요 500
+	 */
+	@PostMapping("/check-id")
+	public ResponseEntity<Void> checkId(String userId) {
+		service.checkId(userId);
+		return ResponseEntity.ok().build();
+	}
+	
+	/**
+	 * 닉네임 중복 확인
+	 * @param nickname
+	 * @return 정상 200, 이상해요 500
+	 */
+	@PostMapping("/check-nickname")
+	public ResponseEntity<Void> checkNickName(String nickname) {
+		service.checkNick(nickname);
+		return ResponseEntity.ok().build();
+	}
 	
 	/**
 	 * 회원 목록
